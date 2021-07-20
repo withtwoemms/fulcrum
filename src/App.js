f4aa1ea1f699b2fe20d163f693fef06ef3e3f14a
Emmanuel I. Obi <withtwoemms@gmail.com>
2021-07-20 13:09:40 -0500

    Layout replaced by Home component in App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Route, Switch } from "react-router-dom";
-import Layout from "./components/Layout";
+import Home from "./components/pages/Home";
 
 function App() {
     const title = 'fulcrum.academy';
@@ -10,24 +10,6 @@ function App() {
         width: window.innerWidth
       }
     );
-    const menuItems = [
-      { icon: `🔒`, text: "Item 1" },
-      { icon: `🔓`, text: "Item 2" },
-      { icon: `🛡️`, text: "Item 3" },
-      { icon: `🔏`, text: "Item 4" },
-      { icon: `🔐`, text: "Item 5" }
-    ];
-    const sidebarCollapsed = windowState.width < 1100;
-    const styles = {
-      white: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-      black: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-      topBarHeight: 40,
-      footerMenuHeight: 50,
-      showFooterMenuText: windowState.width > 500,
-      showSidebar: windowState.width > 768,
-      sidebarCollapsed,
-      sidebarWidth: sidebarCollapsed ? 50 : 150
-    };
 
     useEffect(() => {
       function handleResize() {
@@ -45,20 +27,10 @@ function App() {
       }
     })
 
-    if (styles.showSidebar) {
-      menuItems.push({ icon: `👾`, text: "Profile" });
-      menuItems.push({ icon: `⚙`, text: "Settings" });
-    }
-
-    let styling = {
-      backgroundColor: styles.black(0.05),
-      minHeight: "100vh",
-      position: "relative"
-    }
     return (
       <Switch>
         <Route exact path="/">
-            <Layout styling={styling} menuItems={menuItems} otherStyles={styles} />
+          <Home windowState={windowState} />
         </Route>
       </Switch>
     );
