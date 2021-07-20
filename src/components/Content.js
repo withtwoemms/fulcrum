bdca9315eeb1f8924e0e9174dca3372681a5581c
Emmanuel I. Obi <withtwoemms@gmail.com>
2021-07-20 12:32:33 -0500

    Content made into standalone component

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,15 +1,9 @@
 import React from "react";
 
-const Content = ({ styles }) => {
+const Content = ({ title, text, styles }) => {
   const { showSidebar } = styles;
 
-  const dummyPost = {
-    title: `Here's a blog post title`,
-    summary:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  };
-
-  const posts = Array(20).fill(dummyPost);
+  const post = {title: title, summary: text};
 
   const contentStyle = {
     paddingTop: showSidebar ? 20 : styles.topBarHeight + 20,
@@ -20,14 +14,10 @@ const Content = ({ styles }) => {
 
   return (
     <div style={contentStyle}>
-      {posts.map((post, i) => {
-        return (
-          <div key={i} style={{ marginBottom: 40 }}>
-            <h2 style={{ marginBottom: 0 }}>{post.title}</h2>
-            <p>{post.summary}</p>
-          </div>
-        );
-      })}
+      <div style={{ marginBottom: 40 }}>
+        <h2 style={{ marginBottom: 0 }}>{post.title}</h2>
+        <p>{post.summary}</p>
+      </div>
     </div>
   );
 };
