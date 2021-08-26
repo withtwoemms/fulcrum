21e37a3fd810a473d2c98a3b689a1f772082a899
Emmanuel I. Obi <withtwoemms@gmail.com>
2021-08-25 23:22:22 -0500

    fulcrum#12: "cleanup" (#14)
    * top-level .babelrc overwrites that for subdir
    
    * updates SPA serving function name
    
    * moves README to top-level

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,6 @@ frontend_build = projectroot.joinpath('frontend/build/')
 app = Flask(__name__, static_folder=str(frontend_build), static_url_path='/')
 
 @app.route("/")
-def react_app():
+def deliver_frontend():
     return app.send_static_file('index.html')
 
