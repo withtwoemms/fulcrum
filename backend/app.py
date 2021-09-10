66d172fc310c64f3ed52569fea5bcbef26ae2ec5
Emmanuel I. Obi <withtwoemms@gmail.com>
2021-09-10 07:02:46 -0500

    fulcrum#18: "configuring backend ssl" (#19)
    * first (failed) attempt incorporating certs
    
    * updates SSL config; updates server runtime params informed via envvars

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,39 @@
-from pathlib import Path
+from enum import Enum
 from flask import Flask
+from os import environ as envvars
+from pathlib import Path
 
 
 projectroot = Path(__file__).parent.parent
 frontend_build = projectroot.joinpath('frontend/build/')
+
 app = Flask(__name__, static_folder=str(frontend_build), static_url_path='/')
+
+default_app_env = 'development'
+
+class Environment(Enum):
+    development = default_app_env
+    live = 'live'
+    production = 'production'
+
 
 @app.errorhandler(404)
 def deliver_frontend(error):
     return app.send_static_file('index.html')
+
+
+if __name__ == '__main__':
+    ssl_context = (envvars.get('CERTIFICATE_FILE'), envvars.get('PRIVATEKEY_FILE'))
+    has_valid_ssl_context = all(ssl_context)
+
+    app.env = Environment(envvars.get('APPLICATION_ENV', default_app_env)).value
+
+    params = {
+        'debug': envvars.get('DEBUG', True),
+        'host': envvars.get('HOST', '0.0.0.0'),
+        'port': envvars.get('PORT', 80) if not has_valid_ssl_context else 443,
+        'threaded': envvars.get('THREADED', True)
+    }
+    params.setdefault('ssl_context', ssl_context) if has_valid_ssl_context else None
+    app.run(**params)
 
